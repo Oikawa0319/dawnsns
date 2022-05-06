323d28da9cc867dd96c19ec4595fe2002f17c490
Oikawa0319 <mckskuya@gmail.com>
2022-05-06 18:37:25 +0900

    バリデーション/新規登録機能

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/added';
+    protected $redirectTo = '/add';
 
     /**
      * Create a new controller instance.
@@ -46,14 +46,14 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'username' => 'required|string|max:255',
-            'mail' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:4|confirmed',
-        ]);
-    }
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'username' => 'required|string|max:255',
+    //         'mail' => 'required|string|email|max:255|unique:users',
+    //         'password' => 'required|string|min:4|confirmed',
+    //     ]);
+    // }
 
     /**
      * Create a new user instance after a valid registration.
@@ -61,11 +61,17 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
+
+     //createメソッド/registerメソッドの$data変数をarrayに引数で渡し、$dataとして使用出来る様にする
     protected function create(array $data)
     {
+        //UserモデルでDBのUserテーブルに接続し、追加
         return User::create([
+            //$dataのusernameの値をusernameカラムに追加する
             'username' => $data['username'],
+            //$dataのmailの値をmailカラムに追加する
             'mail' => $data['mail'],
+            //$dataのpasswordの値をpasswordカラムに追加する/bcryptで登録後のpasswordを暗号化する
             'password' => bcrypt($data['password']),
         ]);
     }
@@ -75,17 +81,42 @@ class RegisterController extends Controller
     //     return view("auth.register");
     // }
 
-    public function register(Request $request){
-        if($request->isMethod('post')){
+    //registerメソッド/「register.blade.php」からPOST送信された値を引数の$repuestで受け取る
+    public function register(Request $request)
+    {
+        //$requestの値がpost送信された場合下記を実行する
+        if ($request->isMethod('post')) {
+                //requestの値にバリデーションを設定する
+                $request->validate(
+                    [
+                        //username=入力必須、文字列必須、４文字以上、１２文字以下
+                        'username' => 'required|string|min:4|max:12',
+                        //mail=入力必須、email形式、４文字以上、同じmail登録不可
+                        'mail' => 'required|email|min:4|unique:users,mail',
+                        //password=入力必須、４文字以上、１２文字以下、「password_confirmation」と連携し同じ内容かチェックする
+                        'password' => 'required|min:4|max:12|confirmed',
+                        //password_confirmation=入力必須、４文字以上、１２文字以下
+                        'password_confirmation' => 'required|min:4|max:12',
+                    ],[
+                        //バリデーションが満たされなかった場合のエラー表示
+                        'required' => 'この項目は必須です。',
+                        'min' => '４文字以上入力が必要です。',
+                        'max' => '１２文字以内で入力して下さい。',
+                        'unique' => 'すでに使用されているメールアドレスです。',
+                        'confirmed' => 'パスワードが一致しません。',
+                        ]
+                );
+            //requestの値を取得し$data変数に代入する
             $data = $request->input();
 
+            //createメソットに$dataを引数で渡す
             $this->create($data);
-            return redirect('added');
+            //requestの値のusernameの値を$username変数に代入する
+            $username = $request->input('username');
+            //auth.addedファイルにusernameの値を引き継ぐ
+            return view("auth.added",compact('username'));
         }
-        return view('auth.register');
-    }
-
-    public function added(){
-        return view('auth.added');
+            //バリデーションがエラーの場合auth.registerファイルに移動する
+            return view("auth.register");
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,17 +6,57 @@
 
 <h2>新規ユーザー登録</h2>
 
+<div>
 {{ Form::label('ユーザー名') }}
+<label for="">ユーザー名</label>
 {{ Form::text('username',null,['class' => 'input']) }}
 
+@if($errors->has('username'))
+    <div>
+        <p>{{ $errors->first('username') }}</p>
+    </div>
+@endif
+
+</div>
+
+<div>
 {{ Form::label('メールアドレス') }}
+<label for="">メールアドレス</label>
 {{ Form::text('mail',null,['class' => 'input']) }}
 
+@if($errors->has('mail'))
+    <div>
+        <p>{{ $errors->first('mail') }}</p>
+    </div>
+@endif
+
+</div>
+
+<div>
 {{ Form::label('パスワード') }}
+<label for="">パスワード</label>
 {{ Form::text('password',null,['class' => 'input']) }}
 
+@if($errors->has('password'))
+    <div>
+        <p>{{ $errors->first('password') }}</p>
+    </div>
+@endif
+
+</div>
+
+<div>
 {{ Form::label('パスワード確認') }}
-{{ Form::text('password-confirm',null,['class' => 'input']) }}
+<label for="">パスワード確認</label>
+{{ Form::text('password_confirmation',null,['class' => 'input']) }}
+
+@if($errors->has('password_confirmation'))
+    <div>
+        <p>{{ $errors->first('password_confirmation') }}</p>
+    </div>
+@endif
+
+</div>
 
 {{ Form::submit('登録') }}
 
